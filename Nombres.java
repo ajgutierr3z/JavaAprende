3407019f6cbad9f8f361224debd42608c5e69cbc
system170976 <133520022+system170976@users.noreply.github.com>
2023-06-20 22:28:23 -0600

    nombres
    en este ejercico pedimos al usuario que ingres su nombre y se imprime en pantalla

---
+++ Nombres.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Nombres{
+
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+
+        String Nombre;
+
+        System.out.println("Ingrese su Nombre de usuario: ");
+        Nombre = entrada.nextLine();
+
+
+        System.out.println("Bienvenido al Sistema "+Nombre);
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
