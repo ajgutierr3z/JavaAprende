457c926e898f6b8611850b94763baf4599fe6cc5
system170976 <133520022+system170976@users.noreply.github.com>
2023-06-22 23:19:12 -0600

    Add files via upload
    aqui nos requiere la suma de los elementos del arreglo

---
+++ Promedio.java
@@ -0,0 +1,23 @@
+public class Promedio {
+
+    public static void main(String[] args){
+
+        int[] numeros={19, 17, 21, 9, 12, 7};
+        int suma=0;
+
+
+        for(int i=0; i<6; i++){
+            suma+=numeros[i];
+
+
+        }
+
+        System.out.println("La suma es:"+suma);
+
+
+
+
+    }
+
+
+}
