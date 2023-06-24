e06d40cef656b7f831d184435861ed76b11fdec1
Sony061998 <133594797+Sony061998@users.noreply.github.com>
2023-06-24 11:33:09 -0600

    ejercicios
    solucion de ejercicio resultos en case

---
+++ java/promedio.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int a=55;
+      int b=71;
+      int c=46;
+      int d=87;
+      int p=a+b+c+d;
+
+      System.out.println( "el promedio es = " + p/4);
+    }
+}
