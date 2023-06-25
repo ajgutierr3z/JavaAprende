08296e04365a7bc276b3a5cd1d43c7504cda5cc4
system170976 <133520022+system170976@users.noreply.github.com>
2023-06-24 22:40:46 -0600

    numero mayor
    en esta actividad el programa nos dice que numero es el mayor dl arregloe

---
+++ Mayor.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Mayor {
+
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        int elementos;
+        int mayor=0;
+
+        System.out.println("Ingrese la cantidad de elementos en su arreglo: ");
+        elementos=entrada.nextInt();
+
+        int [] numeros = new int[elementos];
+
+
+        System.out.println("Ingrese los elementos: ");
+        for(int i = 0; i<elementos;i++){
+            System.out.println("Elemento  No. "+(i+1)+" de su arreglo");
+            numeros[i]=entrada.nextInt();
+
+            if(numeros[i]>mayor){
+                mayor=numeros[i];
+
+            }
+
+
+        }
+        System.out.println("El numero mayor de su arreglo es: "+mayor);
+
+    }
+
+
+}
